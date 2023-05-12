82b1e0e9d9c2b9e462861808eed78c924c6b1e40
Javicastropol <119880074+Javicastropol@users.noreply.github.com>
2023-05-12 09:56:49 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+fetch("https://6388b6e5a4bb27a7f78f96a5.mockapi.io/sakura-cards/")
+  .then(Response => Response.json())
+  .then(data => console.log(data))
+  .catch(error => console.log(error))
+
+function App() {
+  return 
+  
+}
+
+
+export default App;
